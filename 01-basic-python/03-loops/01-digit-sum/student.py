96e5538041dcdfc04bcb8d8b3224fa358d6c1bf4
Robbe Peeters <robbe.peeters2@student.ucll.be>
2024-03-22 12:19:59 +0100

    exercise 03 -loops finished

--- 01-basic-python/03-loops/01-digit-sum/student.py
+++ 01-basic-python/03-loops/01-digit-sum/student.py
@@ -1,1 +1,14 @@
-# Write your code here
+def last_digit(n):
+    return n%10
+
+def remove_last_digit(n):
+    return n//10
+
+def digit_sum(n):
+    total = 0
+    i = len(str(n))
+    while i > 0:
+        total += last_digit(n)
+        n = remove_last_digit(n)
+        i -= 1
+    return total
--- 01-basic-python/03-loops/02-is-prime/student.py
+++ 01-basic-python/03-loops/02-is-prime/student.py
@@ -1,1 +1,6 @@
-# Write your code here
+def is_prime(n):
+    for m in range(2, n):
+        if n % m == 0:
+            return False
+
+    return n > 1
