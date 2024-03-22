f7c03bcc55105ed4215141ee99e95740edf07b1c
Robbe Peeters <robbe.peeters2@student.ucll.be>
2024-03-22 11:52:41 +0100

    exercise 02 - conditionals finished

--- 01-basic-python/02-conditionals/03-between/student.py
+++ 01-basic-python/02-conditionals/03-between/student.py
@@ -1,1 +1,8 @@
-# Write your code here
+# def between(x, lower, upper):
+#     if x >= lower:
+#         if x <= upper:
+#             return True
+#     return False
+
+def between(x, lower, upper):
+    return x >= lower and x <= upper
